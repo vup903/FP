bbc16b42b2cc955ca9c823788839b66fca1e2a19
LaiiiBooo <106825320+LaiiiBooo@users.noreply.github.com>
2022-12-13 00:34:36 +0800

    Update main.cpp

--- demoByLai/main.cpp
+++ demoByLai/main.cpp
@@ -1,8 +1,11 @@
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 #include "./maze.h"
-#include "./menu.h"
+#include "./button.h"
+#include "./background.h"
 #include "./ghost.h"
+#include "./pac.h"
+#include "./scoreBoard.h"
 
 using namespace std;
 
@@ -12,18 +15,31 @@ int main()
     sf::Event event;
 
     /*宣告的部分*/
-    /*fileName放圖黨的絕對路徑*/
-    Maze maze("/Users/lbjessie/Desktop/大三上/程式設計/demo/image/磚牆素材.png");
+    /*fileName放圖檔的絕對路徑*/
 
+    //地圖宣告
+    Maze maze("/Users/lbjessie/Desktop/大三上/程式設計/demo/image/磚牆素材.png");
     Ghost que("/Users/lbjessie/Desktop/大三上/程式設計/demo/image/皇后方形.png", 55, 55);
     Ghost ann("/Users/lbjessie/Desktop/大三上/程式設計/demo/image/安陵容方形.png", 1155, 1265);
     Ghost hua("/Users/lbjessie/Desktop/大三上/程式設計/demo/image/華妃方形.png", 1155, 55);
     Ghost chi("/Users/lbjessie/Desktop/大三上/程式設計/demo/image/齊妃方形.png", 55, 1265);
+    Pac pac("/Users/lbjessie/Desktop/大三上/程式設計/demo/image/甄嬛方形.png", 605, 605);
 
-    Menu menu("/Users/lbjessie/Desktop/大三上/程式設計/demo/image/首頁.png");
+    //首頁宣告
+    Background menuImage("/Users/lbjessie/Desktop/大三上/程式設計/demo/image/首頁.png");
     Button startGame("/Users/lbjessie/Desktop/大三上/程式設計/demo/image/startGame.png", 400, 900);
     Button gameRule("/Users/lbjessie/Desktop/大三上/程式設計/demo/image/gameRule.png", 1100, 900);
 
+    //勝利畫面
+    Background winImage("/Users/lbjessie/Desktop/大三上/程式設計/demo/image/勝利畫面.png");
+    Button reStart("/Users/lbjessie/Desktop/大三上/程式設計/demo/image/重新開始.png", 1350, 1200);
+
+    //失敗畫面
+    Background loseImage("/Users/lbjessie/Desktop/大三上/程式設計/demo/image/失敗畫面.png");
+    //Button reStart("/Users/lbjessie/Desktop/大三上/程式設計/demo/image/重新開始.png", 1350, 1200);
+
+    ScoreBoard scoreBoard;
+    float i = 0;
     while (window.isOpen())
     {
         window.clear(sf::Color::Black);
@@ -32,26 +48,36 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-        //如果要看迷宮，就用這段程式碼
+        //看遊戲畫面
         /*
         maze.drawWall(&window);
-         */
-
-
-        //如果要看幽靈，就用這段程式碼，可以跟迷宮一起看
-        /*
         que.drawGhost(&window);
         ann.drawGhost(&window);
         hua.drawGhost(&window);
         chi.drawGhost(&window);
+        pac.drawPac(&window);
+        scoreBoard.drawScoreBoard(i, &window);
          */
 
-        //如果要看首頁，就用這段程式碼，不能跟迷宮和幽靈一起看，會重疊，到時候應該要寫按鈕事件切換
+        //看首頁
         /*
-        menu.drawMenu(&window);
+        menuImage.drawbg(&window);
         startGame.drawButton(&window);
         gameRule.drawButton(&window);
          */
+
+        //看勝利畫面
+
+        winImage.drawbg(&window);
+        reStart.drawButton(&window);
+
+
+        //看失敗畫面
+        /*
+        loseImage.drawbg(&window);
+        reStart.drawButton(&window);
+         */
+
         window.display();
     }
     return 0;
