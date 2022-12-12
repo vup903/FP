65134ce8f43de2e0c537224c55abc3841c4e20b2
LaiiiBooo <106825320+LaiiiBooo@users.noreply.github.com>
2022-12-12 15:14:08 +0800

    Update ghost.h

--- demoByLai/ghost.h
+++ demoByLai/ghost.h
@@ -12,19 +12,23 @@ private:
     sf::Texture ghostTexture;
     sf::Sprite ghostSpr;
 
-    void init(float ghostPos_x, float ghostPos_y) {
-        ghostSpr.setScale(sf::Vector2f(0.1, 0.1));
-        ghostSpr.setPosition(sf::Vector2f(ghostPos_x, ghostPos_y));
-    }
+    void init(float ghostPos_x, float ghostPos_y);
+    
 public:
-    Ghost(string ghostFile, float ghostPos_x, float ghostPos_y) {
-        ghostTexture.loadFromFile(ghostFile);
-        std::cout << "Ghost class OK \n";
-        ghostSpr.setTexture(ghostTexture);
-        init(ghostPos_x, ghostPos_y);
-    };
-
-    void drawGhost(RenderWindow *window) {
-        window->draw(ghostSpr);
-    }
+    Ghost(string ghostFile, float ghostPos_x, float ghostPos_y);
+    void drawGhost(RenderWindow *window) { window->draw(ghostSpr); }
 };
+
+void Ghost::init(float ghostPos_x, float ghostPos_y)
+{
+    ghostSpr.setScale(sf::Vector2f(0.1, 0.1));
+    ghostSpr.setPosition(sf::Vector2f(ghostPos_x, ghostPos_y));
+}
+
+Ghost::Ghost(string ghostFile, float ghostPos_x, float ghostPos_y)
+{
+    ghostTexture.loadFromFile(ghostFile);
+    std::cout << "Ghost class OK \n";
+    ghostSpr.setTexture(ghostTexture);
+    init(ghostPos_x, ghostPos_y);
+}
