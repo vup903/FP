b05dbb4fa76c6a0fac18cd67c65f5033ae61be64
nlnlOeOXD <117833074+nlnlOeOXD@users.noreply.github.com>
2022-12-20 19:56:12 +0800

    Update button.h

--- Lai/FP-main/FP-main/Headers/button.h
+++ Lai/FP-main/FP-main/Headers/button.h
@@ -24,6 +24,7 @@ Button::Button(std::string buttonFile, float x_axis, float y_axis)
 {
     buttonTexture.loadFromFile(buttonFile);
     buttonSprite.setTexture(buttonTexture);
+    buttonSprite.setScale(sf::Vector2f(0.26, 0.26));
     buttonSprite.setPosition(sf::Vector2f(x_axis, y_axis));
     buttonWidth = static_cast<float>(buttonSize.x);
     buttonHeight = static_cast<float>(buttonSize.y);
